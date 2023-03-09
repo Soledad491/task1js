7231bb04ddc038283e17a52c5f2d096d8bff03ca
Soledad491 <soledadfalcondeespinoza@gmail.com>
2023-03-09 07:50:30 -0300

    third commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,30 @@
-let 
+import SearchFilters from "./filterbusq";
+import filterbusq from "./filterbusq";
+
+type="module"
+let eventscard = ["Collectivies Party", "Korean Style", "text", "text", "text", "text", "text", "text", "text" ]
+
+let eventname = document.getElementById("eventname")
+
+let Search = document.getElementById("Search")
+
+ function dibujarlista(arrayeventscard){
+    
+    eventname.innerHTML =""
+    arrayeventscard.forEach((card-title) =>{
+        let listItem = document.createElement("li")
+        listItem.innerHTML = card-title
+    
+        eventname.appendChild("listItem")
+     })
+}
+
+dibujarlista(namee)
+
+Search.addEventListener("change",()=>{
+    let eventosflitrados = namee.filter((namee)=> namee.tolowerCase().incluides(Search.value.tolowerCase()))
+
+    console.log(eventosflitrados);
+})
+
+SearchFilters(".cardfilter",".row")
